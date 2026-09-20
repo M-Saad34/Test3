7533feef5755b7db45d5dc063cb9007257342539
M-Saad34 <saadranaji2012@gmail.com>
2026-09-20 10:17:53 -0700

    Update test3.py

---
+++ test3.py
@@ -0,0 +1,23 @@
+grade_book = {
+    "Ali":85,
+    "Baber":92,
+    "Haider":89,
+    "Saad":93,
+    "Maryam":95,
+}
+
+total_score= 0
+for students in grade_book:
+
+    total_score+=grade_book
+class_average = total_score
+top_scorer =""
+bottom_scorer =""
+print("=====Class students grade")
+print("Class average:")
+print("Top scorer:")
+print("Bottom scorer")
+print("-"*27)
+
+grade= grade_book.get("search_name","Student not found")
+print("Result: grade")
